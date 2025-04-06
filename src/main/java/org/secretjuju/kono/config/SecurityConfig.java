922a2809857abeb47a03b5eae2c1c75ec847a34a
availrum <najojoda@gmail.com>
2025-04-07 03:00:38 +0900

    fix: localpath to prodpath

--- src/main/java/org/secretjuju/kono/config/SecurityConfig.java
+++ src/main/java/org/secretjuju/kono/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration
-				.setAllowedOrigins(Arrays.asList(frontendRedirectUri, "http://localhost:4173", cloudFrontRedirectUri)); // 프론트엔드
+				.setAllowedOrigins(Arrays.asList(frontendRedirectUri, "https://www.playkono.com", cloudFrontRedirectUri)); // 프론트엔드
 																														// 주소
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
